a8465631f6df53bc7ae85ebb27d996150139d4fe
Charles Liu <dodoinblue@gmail.com>
2017-06-03 16:59:28 -0700

    modernize main.js

--- src/main.js
+++ src/main.js
@@ -2,21 +2,17 @@
 import Vue from 'vue'
 
 // Import F7
-import Framework7 from 'framework7'
+import 'framework7'
 
 // Import F7 Vue Plugin
 import Framework7Vue from 'framework7-vue'
 
 // Import F7 iOS Theme Styles
-import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
-import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
-/* OR for Material Theme:
-import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
-import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
-*/
+import 'framework7/dist/css/framework7.ios.min.css'
+import 'framework7/dist/css/framework7.ios.colors.min.css'
 
 // Import App Custom Styles
-import AppStyles from './css/app.css'
+import './css/app.css'
 
 // Import Routes
 import Routes from './routes.js'
@@ -34,12 +30,10 @@ new Vue({
   // Init Framework7 by passing parameters here
   framework7: {
     root: '#app',
-    /* Uncomment to enable Material theme: */
-    // material: true,
     routes: Routes,
   },
   // Register App Component
   components: {
-    app: App
+    App
   }
 });
