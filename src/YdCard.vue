2cbbe7d1fc53c1d66d8cecb8d8a775ea012437ab
Charles Liu <dodoinblue@gmail.com>
2017-06-03 16:59:52 -0700

    Draft: add Yuudee page layouts

---
+++ src/YdCard.vue
@@ -0,0 +1,101 @@
+<template>
+<div class="yd-card" @click="onCardClick()">
+  <div class="card-frame">
+    <img :src="card_bg_image">
+  </div>
+  <div class="card-content">
+    <img :src="image">
+  </div>
+  <div class="card-text"><div>Card: {{card}}</div></div>
+  <div class="card-edit-button"></div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['card'],
+  data() {
+    return {
+      card_bg_image: 'static/img/card_bg.png',
+      image: 'static/card-assets/01/02.xydcard/images/2.jpg',
+      title: 'Card Title',
+      isEditMode: ''
+    };
+  },
+  methods: {
+    onCardClick: function() {
+      console.log("card clicked: " + this.card);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.yd-card {
+  position: relative;
+  max-width: 100%;
+  text-align: center;
+}
+
+.yd-card img {
+  max-width: 100%;
+}
+
+.yd-card .card-frame {
+  z-index: 999;
+}
+
+.yd-card .card-content img {
+  height: 100%;
+  width: 100%;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+}
+
+/*
+  full width = 632
+  full heigh = 612
+
+  content margins:
+  margin top = 46
+  margin left = 77
+  margin right = 81
+  margin bottom = 209
+*/
+.yd-card .card-content {
+  position: absolute;
+  left: 12%;
+  top: 7.3%;
+  right: 12%;
+  bottom: 33.9%
+}
+
+.yd-card .card-text {
+  max-width: 100%;
+  text-align: center;
+  position: absolute;
+  top: 66%;
+  left: 12.18%;
+  right: 12.82%;
+  height: 20%;
+  font-size: 1.0em;
+  z-index: 10;
+  padding-top: 0.2em;
+  color: rgb(80,45,17);
+}
+
+.card-text div {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%); 
+}
+
+.yd-card .card-edit-button {
+  max-width: 30%;
+  content: url("../static/img/edit.png");
+  position: absolute;
+  top: -5%;
+  right: 0%;
+  bottom: 80%;
+}
+</style>
---
+++ src/YdDrawer.vue
@@ -0,0 +1,98 @@
+<template>
+<!--<div class="button button-block button-calm" ng-click="testClick()">Click Me</div>-->
+<!--<div ng-class="{'drawer animated slideInRight': clicked}" ng-show="clicked">-->
+<div class="drawer with-background animated slideInRight">
+  <div class="drawer-back" @click="backClicked()"></div>
+  <f7-swiper class="drawer-content">
+    <f7-swiper-slide class="swiper-slide full-height align-center-vertical" v-for="page in cards">
+      <div class="row" v-for="r in page">
+        <div class="col-50 no-padding" v-for="card in r">
+          <yd-card :card="card"></yd-card>
+        </div>
+      </div>
+    </f7-swiper-slide>
+  </f7-swiper>
+</div>
+</template>
+
+<script>
+import YdCard from './YdCard.vue'
+import Vue from 'vue'
+
+Vue.component('debug', {
+  template: "<!-- debug -->",
+  props: [
+    "item"
+  ],
+  created: function() {
+    console.log("debug", this.item);
+    // debugger
+  }
+});
+
+export default {
+  props: [],
+  components: { YdCard },
+  data() {
+    return {
+      path: '',
+      cards: [ [ ["1", "2"], ["3", "4"] ], [ ["5"] ] ],
+    }
+  },
+  methods: {
+    backClicked: function() {
+      console.log("back clicked");
+    }
+  },
+  computed: {
+    isBase() {
+
+    },
+    isLoaded() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Drawer Layer */
+.drawer {
+  max-width: 885px; /* actual img width */
+  max-height: 810px; /* actual img height */
+  /**height: 810px; !* actual img height - IE7 *!*/
+  /*background-image: url("../../img/child_incat_woodbg.png");*/
+  background-size: cover;
+  background-position: center;
+  position:absolute;
+  left:-10%;
+  top:20%;
+  right: 0;
+}
+
+.drawer.with-background {
+  background-image: url("../static/img/child_incat_woodbg.png");
+}
+/*.drawer:after {*/
+/*content: " ";*/
+/*display: block;*/
+/*width: 100%;*/
+/*padding-top: 91.52%; !* 885:810 ratio *!*/
+/*}*/
+.drawer-content {
+  margin-top: 2.72%;
+  margin-left:10%;
+  margin-bottom: 4.57%;
+  margin-right: 0%;
+  /*position: absolute;*/
+  padding-top: 2.72%;
+}
+
+.drawer-back {
+  position: absolute;
+  content: url("../static/img/child_incat_backbutton.png");
+  height: 16.33%;
+  top: -13.61%;
+  left: 14.88%;
+}
+</style>
