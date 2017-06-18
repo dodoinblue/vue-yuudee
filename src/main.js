34b280d822e3513fc057f25c7776c2d285db72c3
Charles Liu <charles.liu@mioglobal.com>
2017-06-18 11:52:32 -0700

    Add f7 back

--- src/main.js
+++ src/main.js
@@ -1,24 +1,40 @@
 // Import Vue
 import Vue from 'vue'
 
+// Import F7, F7-Vue
+import 'framework7'
+import Framework7Vue from 'framework7-vue'
+
 // Import F7 iOS Theme Styles
 import 'framework7/dist/css/framework7.ios.min.css'
+
+// Import Routes
+import Routes from './routes.js'
+Vue.use(Framework7Vue)
 
 // Import App Component
 import App from './app'
 
 // Stop native context menu
 window.oncontextmenu = function(event) {
-     event.preventDefault();
-     event.stopPropagation();
-     return false;
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
 };
 
-// Init App
-window.app = new Vue({
-  el: '#app',
-  template: '<app/>',
-  components: {
-    App
-  }
-});
+document.addEventListener("deviceready", function(){
+  console.log("device ready");
+  // Init App after device is ready. This is generally fast so no loading modal is needed.
+  var app = new Vue({
+    el: '#app',
+    template: '<app/>',
+    framework7: {
+      root: '#app',
+      routes: Routes,
+    },
+    components: {
+      app: App
+    },
+  });
+  window.app = app;
+}, false);
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 export default [
-  {
-      path: '/about/',
-      component: require('./pages/about.vue')
-  },
-  {
-      path: '/form/',
-      component: require('./pages/about.vue')
-  },
-  {
-      path: '/dynamic-route/blog/:blogId/post/:postId/',
-      component: require('./pages/dynamic-route.vue')
-  }
+//   {
+//       path: '/about/',
+//       component: require('./pages/about.vue')
+//   },
+//   {
+//       path: '/form/',
+//       component: require('./pages/about.vue')
+//   },
+//   {
+//       path: '/dynamic-route/blog/:blogId/post/:postId/',
+//       component: require('./pages/dynamic-route.vue')
+//   }
 ]
