87792b69bcc8f826344856000ab1a442e2e7958d
Charles Liu <dodoinblue@gmail.com>
2017-10-30 23:00:55 -0700

    bump version to 1.3.1

--- src/main.js
+++ src/main.js
@@ -60,7 +60,7 @@ var initApp = function() {
 document.addEventListener('deviceready', /* eslint-disable no-undef */ function() {
   console.log('device ready')
   window.ga.startTrackerWithId('UA-104921838-2', 30)
-  window.ga.setAppVersion('1.3.0')
+  window.ga.setAppVersion('1.3.1')
   window.ga.enableUncaughtExceptionReporting(true)
   db.initDB().then(function() {
     initApp()
